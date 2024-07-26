6909cb943031892d73bdad45037cb0bb768f66cd
Mauricio Rojas <mrojasb2000@gmail.com>
2024-07-25 22:01:52 -0400

    refactor: adding validation rules and validation messages

--- app/Models/ClientModel.php
+++ app/Models/ClientModel.php
@@ -12,7 +12,7 @@ class ClientModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['name', 'username', 'email', 'password', 'picture', 'bio'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
@@ -21,15 +21,27 @@ class ClientModel extends Model
     protected array $castHandlers = [];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
     protected $deletedField  = 'deleted_at';
 
     // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
+    protected $validationRules      = [
+        'name' => 'required|alpha_space|min_length[3]|max_length[75]',
+        'username' => 'required|alpha_space|min_length[8]|max_length[8]',
+        'picture' => 'permit_empty|alpha_numeric',
+        'bio' => 'permit_empty|alpha_numeric',
+        'email' => 'trim|required|valid_email|max_length[85]|xss_clean',
+        'password' => 'trim|required|min_length[6]|max_length[15]|callback_chk_password_expression',
+
+    ];
+    protected $validationMessages   = [
+        'email' => [
+            'valid_email' => 'Email format username@company.com'
+        ]
+    ];
     protected $skipValidation       = false;
     protected $cleanValidationRules = true;
 
@@ -43,4 +55,13 @@ class ClientModel extends Model
     protected $afterFind      = [];
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
+
+    public function chk_password_expression($str) {
+        if (1 !== preg_match("/^.*(?=.{6,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$/", $str)) {
+            $this->form_validation->set_message('chk_password_expression', '%s must be at least 6 characters and must contain at least one lower case letter, one upper case letter and one digit');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    } 
 }
